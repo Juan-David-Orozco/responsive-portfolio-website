138f16d2daa0fd646a40b95397580f5b07bfb814
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-02-15 19:18:47 -0500

    config section - qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,9 +33,6 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 const skillsContent = document.getElementsByClassName('skills__content'),
       skillsHeader = document.querySelectorAll('.skills__header')
 
-console.log(skillsContent)
-console.log(skillsHeader)
-
 function toggleSkills () {
   let itemClass = this.parentNode.className
 
@@ -53,6 +50,24 @@ skillsHeader.forEach((el) => el.addEventListener('click', toggleSkills))
 /* ================  QUALIFICATION SKILLS =================== */
 
 /* ================ QUALIFICATION TABS  =================== */
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+})
 
 /* ================ SERVICES MODAL  =================== */
 
