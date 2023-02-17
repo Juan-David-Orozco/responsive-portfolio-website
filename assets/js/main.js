80d9837cd17910bd7925ea0f0b1634cd442bdd9d
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-02-16 22:18:23 -0500

    setup - scroll-up / scroll-bar / dark-theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,9 +159,41 @@ function scrollHeader () {
 
 window.addEventListener('scroll', scrollHeader)
 
-
-
 /*==================== SHOW SCROLL UP ====================*/ 
+function scrollUp () {
+  const scrollUp = document.getElementById('scroll-up')
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
 
+window.addEventListener('scroll', scrollUp)
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme)  ? 'uil-wind-moon' : 'uil-sun'
+
+
+if(selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-wind-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+
+themeButton.addEventListener('click', () => {
+
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  localStorage.setItem('selected-theme', getCurrentTheme())
+  localStorage.setItem('selected-icon', getCurrentIcon())
+
+})
